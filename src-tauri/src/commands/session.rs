01fbdf7750256579c6ccf12ca326056c1051cf45
Primaphoe24 <11241074@student.itk.ac.id>
2026-09-21 16:04:35 +0800

    fix(M1-25): address review - stub returns Err, add ACL permissions, clarify tz comments

--- src-tauri/src/commands/session.rs
+++ src-tauri/src/commands/session.rs
@@ -1,44 +1,45 @@
 #![allow(unused_variables)]
+
 use uuid::Uuid;
 
 use crate::models::{Session, SessionStatus};
 
 #[tauri::command]
 pub fn create_session(project_id: Uuid, name: Option<String>) -> Result<Session, String> {
-    todo!()
+    Err("create_session: not implemented".to_string())
 }
 
 #[tauri::command]
 pub fn get_session(session_id: Uuid) -> Result<Session, String> {
-    todo!()
+    Err("get_session: not implemented".to_string())
 }
 
 #[tauri::command]
 pub fn get_sessions_by_project(project_id: Uuid) -> Result<Vec<Session>, String> {
-    todo!()
+    Err("get_sessions_by_project: not implemented".to_string())
 }
 
 #[tauri::command]
 pub fn update_session_name(session_id: Uuid, new_name: String) -> Result<Session, String> {
-    todo!()
+    Err("update_session_name: not implemented".to_string())
 }
 
 #[tauri::command]
 pub fn update_session_status(session_id: Uuid, status: SessionStatus) -> Result<Session, String> {
-    todo!()
+    Err("update_session_status: not implemented".to_string())
 }
 
 #[tauri::command]
 pub fn assign_image_to_session(session_id: Uuid, image_id: Uuid) -> Result<(), String> {
-    todo!()
+    Err("assign_image_to_session: not implemented".to_string())
 }
 
 #[tauri::command]
 pub fn recalculate_session_date_range(session_id: Uuid) -> Result<(), String> {
-    todo!()
+    Err("recalculate_session_date_range: not implemented".to_string())
 }
 
 #[tauri::command]
 pub fn delete_session(session_id: Uuid) -> Result<(), String> {
-    todo!()
+    Err("delete_session: not implemented".to_string())
 }
