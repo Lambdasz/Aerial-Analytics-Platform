f5b26d615729db4fedb945c786cfeb2bee7d7e8a
Muhammad Zaini <muhammadzaini1118@gmail.com>
2026-09-22 03:54:56 +0800

    Revert "Feat/cacing2 naga2 modul2 (#21)"
    This reverts commit 49b487a3c8b4fbde214914f923647e6848b61db8.

--- src/component_plugin_manager/index.ts
+++
@@ -1,8 +0,0 @@
-export { PluginManager } from "./PluginManager";
-export { processExecutionResult, validateAndCreatePayload } from "./pluginCommands";
-export type {
-  ExecutionResult,
-  ImageMetadata,
-  PluginInputsRequirement,
-  PluginPayload,
-} from "./pluginCommands";
