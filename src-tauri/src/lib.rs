1bff3dfee071dad7ccb7fe88aa4377d532a9626a
dvaanf <dvaalzaa@gmail.com>
2026-09-22 03:15:47 +0800

    feat(module-10): add rust cross-module functions

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,8 @@
 mod plugin_manager;
+mod modules;
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
